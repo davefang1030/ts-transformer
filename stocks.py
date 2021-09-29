069985301a7aee7d4a5c5f93cfe8747b82db0d5d
davefang1030 <88542515+davefang1030@users.noreply.github.com>
2021-09-28 22:24:46 -0400

    add different scalers for different data set

--- stocks.py
+++ stocks.py
@@ -1,12 +1,22 @@
 from data import PdTorchDataset
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
 class StockDataset(PdTorchDataset):
+    """
+    StockDataset represent dataframes with multiple columns of time series data. First column is date and last column
+    is split to indicate train/val/test entries. It provides data to be trained by transformer (train on historical
+    data with lookback_window length and predict future time series with forward_window length:
+        x_source (time series with length of lookback_window), inputs to encoder side
+        y_target (time series with length of forward_window), outputs of the decoder
+        y_input (time series with length of forward_window), y_target shifted right one spot with last from x_source
+    """
+
     def __init__(self, df, lookback_window, forward_window, src_cols, tgt_cols):
         """
-
-        :param df: dataframe that holds data
+        :param df: dataframe that holds data. All train/val/test data should be continuous wrt dates.
+                   we cannot randomize them when we create train/val/test labels.
         :param lookback_window: look back window for source
         :param forward_window: prediction period
         :param src_cols: list of column names for source
@@ -19,22 +29,35 @@ class StockDataset(PdTorchDataset):
         self.src_cols = src_cols
         self.tgt_cols = tgt_cols
 
+        self.train_scaler = MinMaxScaler(feature_range=(-1, 1))
+        self.val_scaler = MinMaxScaler(feature_range=(-1, 1))
+        self.test_scaler = MinMaxScaler(feature_range=(-1, 1))
+        # scale the data and ignore first column (date) and last column (split)
+        df1 = self.train_scaler.fit_transform(self.train_df.iloc[:, 1:-1])
+        self.train_df.iloc[:, 1:-1] = df1
+        df1 = self.val_scaler.fit_transform(self.val_df.iloc[:, 1:-1])
+        self.val_df.iloc[:, 1:-1] = df1
+        df1 = self.test_scaler.fit_transform(self.test_df.iloc[:, 1:-1])
+        self.test_df.iloc[:, 1:-1] = df1
+
+    def __len__(self):
+        # override base and return dataframe size minus lookback_window and forward_window
+        return len(self._target_df) - self.lookback_window - self.forward_window
+
     def __getitem__(self, index):
         """
         starting from index, there are lookback_window obs as input, followed by forward_window obs as target
         :param index:
         :return:
         """
-        row_idx = self._target_df.index[index]
-        idx = list(self.df.index).index(row_idx)
-
-        df1 = self.df[idx:idx+self.lookback_window]
+        df1 = self._target_df[index:index+self.lookback_window]
         x_source = df1.loc[:, self.src_cols].to_numpy().astype(np.float32)
 
-        df1 = self.df[idx+self.lookback_window:idx+self.lookback_window+self.forward_window]
+        df1 = self._target_df[index+self.lookback_window:index+self.lookback_window+self.forward_window]
         y_target = df1.loc[:, self.tgt_cols].to_numpy().astype(np.float32)
 
-        df1 = self.df[idx+self.lookback_window-self.forward_window:idx+self.lookback_window]
+        # decoder input is the target shifted to right with first element from last element of x_source
+        df1 = self._target_df[index+self.lookback_window-1:index+self.lookback_window+self.forward_window-1]
         y_input = df1.loc[:, self.tgt_cols].to_numpy().astype(np.float32)
 
         return {"x_source": x_source,
@@ -43,29 +66,36 @@ class StockDataset(PdTorchDataset):
 
 
 class StockDatasetGPT(PdTorchDataset):
-    def __init__(self, df, sequence_length, src_cols, tgt_cols):
+    """
+    StockDatasetGPT represent dataframes with multiple columns of time series data. First column is date and last is
+    split to indicate train/val/test entries. It provides data to be trained by decoder only (GPT) type of transformer
+    (train on historical data with sequence_length length and predict one step future time series:
+        y_input (time series with length of sequence_length), the target is shifted right one spot with y_input with
+        last element from original series that comes after y_input
+    """
+    def __init__(self, df, sequence_length, src_cols):
         """
-        :param df: dataframe that holds data
+        :param df: dataframe that holds data. All train/val/test data should be continuous wrt dates.
+                   we cannot randomize them when we create train/val/test labels.
         :param sequence_length: time series length for training
-        :param src_cols: list of column names for source
-        :param tgt_cols: list of column names for target
+        :param src_cols: list of column names for source. There is no different tgt_cols as this implementation
+                    is meant to test autoregressive feature and teacher forcing
         """
         # no need for a vectorizer
         super().__init__(df, vectorizer=None)
         self.sequence_length = sequence_length
         self.src_cols = src_cols
-        self.tgt_cols = tgt_cols
+
+    def __len__(self):
+        # override base and return dataframe size minus sequence_length
+        return len(self._target_df) - self.sequence_length
 
     def __getitem__(self, index):
         """
         :param index:
         :return:
         """
-        row_idx = self._target_df.index[index]
-        idx = list(self.df.index).index(row_idx)
-
-        df1 = self.df[idx:idx+self.sequence_length]
+        df1 = self._target_df[index:index+self.sequence_length]
         y_input = df1.loc[:, self.src_cols].to_numpy().astype(np.float32)
 
         return {"y_input": y_input}
-
